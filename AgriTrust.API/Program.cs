395f8a34e25acae94c38071701ca2f5e37eb7d3d
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-02-10 13:02:15 +0530

    Add MySQL database context configuration and update appsettings for connection string

--- AgriTrust.API/Program.cs
+++ AgriTrust.API/Program.cs
@@ -1,14 +1,23 @@
+using AgriTrust.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AgriTrustDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    )
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
