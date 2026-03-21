f7617af2fad6d5bbddecc3ceff662fb36fc5fb25
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-03-21 13:11:47 +0530

    Implement the Hash helpers for Usage of Certificateservices and  implement Certificate.cs model.

---
+++ AgriTrust.API/Models/Certificate.cs
@@ -0,0 +1,16 @@
+namespace AgriTrust.API.Models;
+
+public class Certificate
+{
+    public int Id { get; set; }
+
+    public string CertificateNumber { get; set; }
+
+    public int FarmerId { get; set; }
+
+    public DateTime IssuedAt { get; set; }
+
+    public DateTime ExpiryDate { get; set; }
+
+    public string Hash { get; set; }
+}
