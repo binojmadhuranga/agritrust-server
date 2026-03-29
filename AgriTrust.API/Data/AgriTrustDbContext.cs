73100da92e6468a9f410c8e1deaf52bc8ad74335
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-03-29 19:41:11 +0530

    Update the relationship between the User (Farmer) and Certificate entities.

--- AgriTrust.API/Data/AgriTrustDbContext.cs
+++ AgriTrust.API/Data/AgriTrustDbContext.cs
@@ -19,6 +19,18 @@ namespace AgriTrust.API.Data
             modelBuilder.Entity<CertificateRequest>()
                 .Property(c => c.Status)
                 .HasConversion<string>();
+            
+            // One-to-One Relationship: User (Farmer) <-> Certificate
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Certificate)
+                .WithOne(c => c.Farmer)
+                .HasForeignKey<Certificate>(c => c.FarmerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Farmer can have only one certificate
+            modelBuilder.Entity<Certificate>()
+                .HasIndex(c => c.FarmerId)
+                .IsUnique();
         }
         
     }
